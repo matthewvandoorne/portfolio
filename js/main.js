4dab9613a2dc73e97e46585605dd1550f2da9736
unknown <baileylievens@hotmail.be>
2023-08-29 22:52:03 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Navigation toggle
+
+let menuToggle = document.querySelector('.hamburger');
+let menuState = document.getElementById('HamburgerState');
+
+menuToggle.addEventListener('click', function() {
+    menuState.click();
+});
