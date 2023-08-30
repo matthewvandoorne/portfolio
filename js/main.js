126fd18edc3ab746753035346b2c4aaf463e22ce
unknown <baileylievens@hotmail.be>
2023-08-30 20:01:55 +0200

    Fixed horizontal scroll on mobile

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@
 let menuToggle = document.querySelector('.hamburger');
 let menuState = document.getElementById('HamburgerState');
 let body = document.querySelector('body');
+let html = document.querySelector('html');
 
 menuToggle.addEventListener('click', function() {
     menuState.click();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 
     if (menuState.checked) {
+        html.style.overflowY = 'hidden';
         body.style.overflowY = 'hidden';
     } else {
+        html.style.overflowY = 'auto';
         body.style.overflowY = 'auto';
     }
 });
