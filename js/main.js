2195ce43731ec36b2d28bbf36f3051359d40be37
unknown <baileylievens@hotmail.be>
2023-08-30 19:41:26 +0200

    Fixed nav not scrolling on mobile

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 let menuToggle = document.querySelector('.hamburger');
 let menuState = document.getElementById('HamburgerState');
+let body = document.querySelector('body');
 
 menuToggle.addEventListener('click', function() {
     menuState.click();
+    body.style.overflowY = "hidden";
 });
